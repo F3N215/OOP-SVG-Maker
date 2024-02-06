b72341cb3f278920ceca49f67ec3a680ff6aa739
Daniel John <dcampo85@gmail.com>
2024-02-06 16:54:59 -0500

    add questions array

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+
+// questions array for user input
+const questions = [
+  {
+    type: "confirm",
+    name: "confirm",
+    message: "Ready to make some shapes?",
+  },
+  {
+    type: "input",
+    name: "text",
+    message: "Text: Enter up to (3) Characters:",
+  },
+  {
+    type: "input",
+    name: "text-color",
+    message: "Text Color: Enter a color keyword, or use a HEX code:",
+  },
+  {
+    type: "input",
+    name: "shape",
+    message: "Shape Color: Enter a color keyword, or use a HEX code:",
+  },
+  {
+    type: "list",
+    name: "pixel-image",
+    message: "Choose which pixel image you would like?",
+    choices: ["Circle", "Square", "Triangle"],
+  },
+];
